40a50561da187270f46e5d4a7c615452b429bcb0
adityaKumarModak <adityakrmodak@gmail.com>
2024-04-07 13:30:20 +0530

    feat: google auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleStrategy } from './google.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [PassportModule],
+  providers: [AuthService, GoogleStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
