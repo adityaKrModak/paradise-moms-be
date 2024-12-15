815e2a758b23e189b63921d79d0abf6d1f7f105e
adityaKumarModak <adityakrmodak@gmail.com>
2024-12-15 12:55:15 +0530

    refactor: restructure database schema and update user resolver for enhanced functionality
    - Removed the old migration file and created a new migration with updated database schema, including enums for OrderStatus and Role, and new fields in User and Product tables.
    - Updated UsersResolver to replace the findOne and getUserByEmail queries with a new getCurrentUser query, allowing users to retrieve their own profile.
    - Enhanced updateUser mutation to restrict profile updates to the authenticated user only.

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,8 +1,8 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User, UserRole } from './entities/user.entity';
 import { UpdateUserInput } from './dto/update-user.input';
-import { UseGuards } from '@nestjs/common';
+import { Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
@@ -12,17 +12,9 @@ export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.usersService.findOne(id);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Query(() => User, { name: 'userByEmail' })
-  async getUserByEmail(
-    @Args('email', { type: () => String }) email: string, // Accept an 'email' argument
-  ) {
-    return this.usersService.findUserByEmail(email);
+  @Query(() => User, { name: 'me' })
+  async getCurrentUser(@Request() req) {
+    return this.usersService.findOne(req.user.userId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
@@ -33,8 +25,15 @@ export class UsersResolver {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+  @Mutation(() => User, { name: 'updateUserInput' })
+  updateUser(
+    @Request() req,
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+  ) {
+    const userId = req.user.userId;
+    if (updateUserInput.id !== userId) {
+      throw new Error('You can only update your own profile');
+    }
+    return this.usersService.update(userId, updateUserInput);
   }
 }
