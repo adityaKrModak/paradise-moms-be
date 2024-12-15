a753e9aa9d43533866af821ab5c94ec387ea92b2
adityaKumarModak <adityakrmodak@gmail.com>
2024-12-15 12:13:58 +0530

    feat: make modules global and export services for better accessibility
    - Added @Global() decorator to PrismaModule and AuthModule to allow their services to be injected across the application.
    - Exported UsersService from UsersModule to enable its usage in other modules.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,16 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { forwardRef, Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
 
+@Global()
 @Module({
   imports: [
+    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
